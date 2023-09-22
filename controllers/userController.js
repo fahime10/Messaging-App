76757e65258c26df19998368fff8f2b3902ffe4d
Fahim Ahmed <p2595607@my365.dmu.ac.uk>
2023-09-22 01:52:56 +0100

    Implemented sign up functionality

---
+++ controllers/userController.js
@@ -0,0 +1,75 @@
+const User = require('../models/userModel');
+const passport = require('passport');
+const asyncHandler = require('express-async-handler');
+const { body, validationResult } = require('express-validator');
+const bcrypt = require('bcryptjs');
+
+exports.sign_up_form_get = asyncHandler(async (req, res, next) => {
+    res.render('sign_up_form', { 
+        title: 'Whatsapp sign up form',
+        content: 'content' 
+    });
+});
+
+exports.sign_up_form_post = [
+    body('first-name')
+        .trim()
+        .escape(),
+
+    body('last-name')
+        .trim()
+        .escape(),
+
+    body('username')
+        .custom(async value => {
+            const user = await User.findOne({ username: `${value}` });
+
+            if (user) {
+                throw new Error('Username already in use');
+            }
+        })
+        .trim()
+        .escape(),
+
+    body('confirm_password', 'Passwords do not match')
+        .custom((value, {req}) => {
+            return value === req.body.password;
+        })
+        .escape(),
+
+
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        try {
+            let password = req.body.password;
+
+            bcrypt.hash(password, 10, async (err, hashedPassword) => {
+                if (err) {
+                    return;
+                } else {
+                    const user = new User({
+                        firstName: req.body.first_name,
+                        lastName: req.body.last_name,
+                        username: req.body.username,
+                        password: hashedPassword
+                    });
+
+                    if (!errors.isEmpty()) {
+                        res.render('sign_up_form', {
+                            title: 'Whatsapp',
+                            user: user,
+                            content: 'content',
+                            errors: errors.array()
+                        });
+                    } else {
+                        await user.save();
+                        res.redirect('/');
+                    }
+                }
+            });
+        } catch (err) {
+            return next(err);
+        }
+    })
+];
--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 const index_controller = require('../controllers/indexController');
+const user_controller = require('../controllers/userController');
 
 router.get('/', index_controller.index);
+
+router.get('/sign-up', user_controller.sign_up_form_get);
+
+router.post('/sign-up', user_controller.sign_up_form_post);
 
 module.exports = router;
